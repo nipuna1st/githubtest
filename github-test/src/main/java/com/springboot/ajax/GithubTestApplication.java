6d3ef551184a8fd90d2d3a5f663e4bdd37dd49f5
Nipuna Madushan <Nipuna_lspl@dialog.dialoggsm.com>
2018-06-20 16:38:26 +0530

    projected added

---
+++ github-test/src/main/java/com/springboot/ajax/GithubTestApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.ajax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTestApplication.class, args);
+	}
+}
